b1c2740f4a64ee55fd5a617e4048787e4721b59c
aditya dubey <adityaxmom112005@gmail.com>
2024-10-12 01:46:21 +0530

    upadate code

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,38 @@
 import React from 'react'
 
+import HeaderBox from '@/components/HeaderBox'
+import TotalBalancebox from '@/components/TotalBalancebox'
+import RightSidebar from '@/components/RightSidebar'
 const Home = () => {
+  const loggedIn = { firstName: 'Aditya', lastName: 'Dubey', 
+    email: 'adityakrdubey456@gmail.com'};
   return (
-    <div>Home</div>
+    <section className='home'>
+      <div className='home-content'>
+        <header className='home-header'>
+         <HeaderBox 
+           type="greeting"
+           title="Welcome"
+           user={loggedIn?.firstName || 'Guest'}
+           subtext="Access and manage your account and transaction efficiently"
+         />
+
+         <TotalBalancebox 
+          accounts={[]}
+          totalBanks={1}
+          totalCurrentBalance={1258.35}
+         />
+        </header>
+        RECENT TRANSACTION
+      </div>
+
+      <RightSidebar 
+      user={loggedIn}
+      transactions={[]}
+      banks={[{ currentBalance: 123.50 }, { currentBalance: 134.67}]}
+      />
+
+    </section>
   )
 }
 
